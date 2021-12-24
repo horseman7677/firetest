0542ec2a87ff0f395804a85a11d481e0071d1614
horseman7677 <70053149+horseman7677@users.noreply.github.com>
2021-12-24 19:41:37 +0530

    firetest2

--- app/src/main/java/com/example/firetest/MainActivity.java
+++ app/src/main/java/com/example/firetest/MainActivity.java
@@ -17,6 +17,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+    TextView data;
     EditText email,phone,text;
     Button btn;
     FirebaseDatabase rootNode;
@@ -27,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        data = findViewById(R.id.data);
         email = findViewById(R.id.email);
         phone = findViewById(R.id.phone);
         text = findViewById(R.id.ptext);
@@ -43,11 +44,28 @@ public class MainActivity extends AppCompatActivity {
 
                 rootNode = FirebaseDatabase.getInstance();
                 reference = rootNode.getReference("users");
+                retriveData();
 
                 FetcherBase fetcherBase = new FetcherBase(email1,phone1,text1);
                 reference.child(phone1).setValue(fetcherBase);
 
                 Toast.makeText(getApplicationContext(), "uploaded", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void retriveData() {
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String val = snapshot.getValue(String.class);
+
+                data.setText(val);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(MainActivity.this, "error...", Toast.LENGTH_SHORT).show();
             }
         });
     }
